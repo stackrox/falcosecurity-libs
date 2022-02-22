49bace87ecd3ac031357ea2e4e073fec7cf9ad81
Giles Hutton <ghutton@redhat.com>
2022-02-22 10:49:48 +0000

    ROX-9409: removes PROBE_NAME and PROBE_VERSION in favour of DRIVER_NAME and DRIVER_VERSION used by userspace/libscap/CMakeLists.txt

--- cmake/modules/libscap.cmake
+++ cmake/modules/libscap.cmake
@@ -15,23 +15,6 @@ get_filename_component(DRIVER_CONFIG_DIR ${CMAKE_BINARY_DIR}/driver/src ABSOLUTE
 get_filename_component(LIBSCAP_INCLUDE_DIR ${LIBSCAP_DIR}/userspace/libscap ABSOLUTE)
 set(LIBSCAP_INCLUDE_DIRS ${LIBSCAP_INCLUDE_DIR} ${DRIVER_CONFIG_DIR})
 
-if(CMAKE_SYSTEM_NAME MATCHES "Linux")
-	if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-		set(KBUILD_FLAGS "${FALCOSECURITY_LIBS_DEBUG_FLAGS}")
-	endif()
-
-    if(NOT DEFINED PROBE_VERSION)
-        set(PROBE_VERSION "${SYSDIG_VERSION}")
-    endif()
-    if(NOT DEFINED PROBE_NAME)
-        set(PROBE_NAME "collector")
-    endif()
-
-    if(NOT DEFINED PROBE_DEVICE_NAME)
-        set(PROBE_DEVICE_NAME "sysdig")
-    endif()
-endif()
-
 if(BUILD_USERSPACE)
 	add_subdirectory(${LIBSCAP_DIR}/userspace/libscap ${PROJECT_BINARY_DIR}/libscap)
 elseif(((BUILD_DRIVER) OR (BUILD_BPF)) AND (CMAKE_SYSTEM_NAME MATCHES "Linux"))
