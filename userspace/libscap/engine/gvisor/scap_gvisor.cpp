LucaGuerra/libs

new(gvisor): add ability to create a new trace session for a given running sandbox
Signed-off-by: Lorenzo Susini <susinilorenzo1@gmail.com>

userspace/libscap/engine/gvisor/scap_gvisor.cpp
@@ -400,4 +400,22 @@ std::vector<std::string> engine::runsc_list()
 	return sandboxes;
 }
 
+void engine::runsc_trace_create(std::string &sandbox_id)
+{
+	const char *argv[] = {
+		"runsc", 
+		"--root",
+		m_root_path.c_str(),
+		"trace",
+		"create",
+		"--force",
+		"--config", 
+		m_podinit_path.c_str(),
+		sandbox_id.c_str(),
+		NULL
+	};
+
+	std::vector<std::string> output = runsc((char **)argv);
+}
+
 } // namespace scap_gvisor

stackrox/falcosecurity-libs

userspace/libscap/engine/gvisor/scap_gvisor.cpp
@@ -472,4 +472,22 @@ std::vector<std::string> engine::runsc_list()
 	return sandboxes;
 }
 
+void engine::runsc_trace_create(std::string &sandbox_id)
+{
+	const char *argv[] = {
+		"runsc", 
+		"--root",
+		m_root_path.c_str(),
+		"trace",
+		"create",
+		"--force",
+		"--config", 
+		m_trace_session_path.c_str(),
+		sandbox_id.c_str(),
+		NULL
+	};
+
+	std::vector<std::string> output = runsc((char **)argv);
+}
+
 } // namespace scap_gvisor