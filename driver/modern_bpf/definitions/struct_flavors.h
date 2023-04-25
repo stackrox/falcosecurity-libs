b27c157684a42c3a2257e62043ad92e7ee4d5b8c
Dmitrii Dolgov <9erthalion6@gmail.com>
2023-04-25 08:36:33 +0200

    Move task_struct__cos into clang attribute section
    Otherwise __attribute__((preserve_access_index)) will not be applied.
    
    Signed-off-by: Dmitrii Dolgov <9erthalion6@gmail.com>

--- driver/modern_bpf/definitions/struct_flavors.h
+++ driver/modern_bpf/definitions/struct_flavors.h
@@ -14,8 +14,43 @@ struct mm_struct___v6_2
 	struct percpu_counter rss_stat[NR_MM_COUNTERS];
 };
 
+/* COS kernels handle audit field differently, see [1]. To support both
+ * versions define COS subset of task_struct with a flavor suffix (which will
+ * be ignored during relocation matching [2]).
+ *
+ * [1]: https://chromium.googlesource.com/chromiumos/third_party/kernel/+/096925a44076ba5c52faa84d255a847130ff341e%5E%21/#F2
+ * [2]: https://git.kernel.org/pub/scm/linux/kernel/git/bpf/bpf-next.git/tree/tools/lib/bpf/libbpf.c#n5347
+ */
+struct audit_task_info {
+	kuid_t			loginuid;
+	unsigned int		sessionid;
+	struct audit_context	*ctx;
+};
+
+struct task_struct___cos {
+	struct audit_task_info		*audit;
+};
+
 #ifndef BPF_NO_PRESERVE_ACCESS_INDEX
 #pragma clang attribute pop
 #endif
+
+/* We don't need relocation on these structs, they are internally defined by us as a fallback!
+ * Use the prefix `modern_bpf__` before the real name of the struct we want to replace.
+ */
+
+/* We use this as a fallback for kernels where `struct __kernel_timespec` is not defined. */
+struct modern_bpf__kernel_timespec
+{
+	long int tv_sec;
+	long int tv_nsec;
+};
+
+/* We use this as a fallback for kernels where `struct __kernel_timex_timeval` is not defined. */
+struct modern_bpf__kernel_timex_timeval
+{
+	long long int tv_sec;
+	long long int tv_usec;
+};
 
 #endif /* __STRUCT_FLAVORS_H__ */
