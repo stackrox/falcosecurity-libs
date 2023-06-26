a8a3a0b2bb29e98473b2d1ade309bbc8d4f0e0df
JoukoVirtanen <jouko@stackrox.com>
2023-06-26 14:14:24 -0700

    Commented out some of fillers openat_x

--- driver/bpf/fillers.h
+++ driver/bpf/fillers.h
@@ -3216,6 +3216,7 @@ FILLER(sys_openat_x, true)
 	long retval;
 	int res;
 
+	res = 0;
 	retval = bpf_syscall_get_retval(data->ctx);
 	res = bpf_val_to_ring(data, retval);
 	if (res != PPM_SUCCESS)
@@ -3268,10 +3269,10 @@ FILLER(sys_openat_x, true)
 	if (res != PPM_SUCCESS)
 		return res;
 
-	/*
-	 * Ino
-	 */
-	res = bpf_val_to_ring(data, ino);
+	///*
+	// * Ino
+	// */
+	//res = bpf_val_to_ring(data, ino);
 	return res;
 }
 
