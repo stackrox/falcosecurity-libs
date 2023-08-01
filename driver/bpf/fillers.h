falcosecurity/libs

fix(driver): fixed 6.3 kernel build and verifier issues.
Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

driver/bpf/fillers.h
@@ -2704,17 +2704,29 @@ FILLER(execve_family_flags, true)
 
 	/* Parameter 21: cap_inheritable (type: PT_UINT64) */
 	kernel_cap_t cap = _READ(cred->cap_inheritable);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	res = bpf_push_u64_to_ring(data, capabilities_to_scap(((unsigned long)cap.cap[1] << 32) | cap.cap[0]));
+#else
+	res = bpf_push_u64_to_ring(data, capabilities_to_scap((unsigned long)cap.val));
+#endif
 	CHECK_RES(res);
 
 	/* Parameter 22: cap_permitted (type: PT_UINT64) */
 	cap = _READ(cred->cap_permitted);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	res = bpf_push_u64_to_ring(data, capabilities_to_scap(((unsigned long)cap.cap[1] << 32) | cap.cap[0]));
+#else
+	res = bpf_push_u64_to_ring(data, capabilities_to_scap((unsigned long)cap.val));
+#endif
 	CHECK_RES(res);
 
 	/* Parameter 23: cap_effective (type: PT_UINT64) */
 	cap = _READ(cred->cap_effective);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	res = bpf_push_u64_to_ring(data, capabilities_to_scap(((unsigned long)cap.cap[1] << 32) | cap.cap[0]));
+#else
+	res = bpf_push_u64_to_ring(data, capabilities_to_scap((unsigned long)cap.val));
+#endif
 	CHECK_RES(res);
 
 	/* Parameter 24: exe_file ino (type: PT_UINT64) */
@@ -5754,19 +5766,31 @@ FILLER(sys_capset_x, true)
 	struct cred *cred = (struct cred*) _READ(task->cred);
 
 	cap = _READ(cred->cap_inheritable);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	val = ((unsigned long)cap.cap[1] << 32) | cap.cap[0];
+#else
+	val = (unsigned long)cap.val;
+#endif
 	res = bpf_push_u64_to_ring(data, capabilities_to_scap(val));
 	if(unlikely(res != PPM_SUCCESS))
 		return res;
 
 	cap = _READ(cred->cap_permitted);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	val = ((unsigned long)cap.cap[1] << 32) | cap.cap[0];
+#else
+	val = (unsigned long)cap.val;
+#endif
 	res = bpf_push_u64_to_ring(data, capabilities_to_scap(val));
 	if(unlikely(res != PPM_SUCCESS))
 		return res;
 
 	cap = _READ(cred->cap_effective);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	val = ((unsigned long)cap.cap[1] << 32) | cap.cap[0];
+#else
+	val = (unsigned long)cap.val;
+#endif
 	return bpf_push_u64_to_ring(data, capabilities_to_scap(val));
 }
 

stackrox/falcosecurity-libs

driver/bpf/fillers.h
@@ -2955,17 +2955,32 @@ FILLER(execve_family_flags, true)
 
 	/* Parameter 21: cap_inheritable (type: PT_UINT64) */
 	kernel_cap_t cap = _READ(cred->cap_inheritable);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	res = bpf_val_to_ring(data, capabilities_to_scap(((unsigned long)cap.cap[1] << 32) | cap.cap[0]));
+#else
+	res = bpf_val_to_ring(data, capabilities_to_scap((unsigned long)cap.val));
+#endif
 	CHECK_RES(res);
 
 	/* Parameter 22: cap_permitted (type: PT_UINT64) */
 	cap = _READ(cred->cap_permitted);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	res = bpf_val_to_ring(data, capabilities_to_scap(((unsigned long)cap.cap[1] << 32) | cap.cap[0]));
+#else
+	res = bpf_val_to_ring(data, capabilities_to_scap((unsigned long)cap.val));
+#endif
 	CHECK_RES(res);
 
 	/* Parameter 23: cap_effective (type: PT_UINT64) */
 	cap = _READ(cred->cap_effective);
 	res = bpf_val_to_ring(data, capabilities_to_scap(((unsigned long)cap.cap[1] << 32) | cap.cap[0]));
+=======
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
+	res = bpf_push_u64_to_ring(data, capabilities_to_scap(((unsigned long)cap.cap[1] << 32) | cap.cap[0]));
+	res = bpf_val_to_ring(data, capabilities_to_scap(((unsigned long)cap.cap[1] << 32) | cap.cap[0]));
+#else
+	res = bpf_val_to_ring(data, capabilities_to_scap((unsigned long)cap.val));
+#endif
 	CHECK_RES(res);
 
 #if LINUX_VERSION_CODE > KERNEL_VERSION(4, 9, 0)
@@ -6101,19 +6116,31 @@ FILLER(sys_capset_x, true)
 	struct cred *cred = (struct cred*) _READ(task->cred);
 
 	cap = _READ(cred->cap_inheritable);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	val = ((unsigned long)cap.cap[1] << 32) | cap.cap[0];
+#else
+	val = (unsigned long)cap.val;
+#endif
 	res = bpf_val_to_ring(data, capabilities_to_scap(val));
 	if(unlikely(res != PPM_SUCCESS))
 		return res;
 
 	cap = _READ(cred->cap_permitted);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	val = ((unsigned long)cap.cap[1] << 32) | cap.cap[0];
+#else
+	val = (unsigned long)cap.val;
+#endif
 	res = bpf_val_to_ring(data, capabilities_to_scap(val));
 	if(unlikely(res != PPM_SUCCESS))
 		return res;
 
 	cap = _READ(cred->cap_effective);
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 	val = ((unsigned long)cap.cap[1] << 32) | cap.cap[0];
+#else
+	val = (unsigned long)cap.val;
+#endif
 	res = bpf_val_to_ring(data, capabilities_to_scap(val));
 	if(unlikely(res != PPM_SUCCESS))
 		return res;