ceb9a042aba40638e9a272ccefa33be2e3daa5cf
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2023-09-05 10:11:23 +0200

    build(minimal-build): add MINIMAL_BUILD_WITH_EBPF option (#56)

--- userspace/libsinsp/wrapper.h
+++ userspace/libsinsp/wrapper.h
@@ -6,6 +6,5 @@
 #include "sinsp.h"
 
 sinsp* SINSP_PUBLIC new_inspector();
-sinsp_chisel* SINSP_PUBLIC new_chisel(sinsp* inspector, const std::string& name, bool is_file = true);
 
 #endif  // _WRAPPER_H_
