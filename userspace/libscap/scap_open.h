2a3fb5f707dd541ec7de081643ef9907934af390
Matthew Knight <matthew.knight@sysdig.com>
2023-10-05 22:56:10 -0700

    incorporate libpman to libsinsp logging
    Signed-off-by: Matthew Knight <matthew.knight@sysdig.com>

--- userspace/libscap/scap_open.h
+++ userspace/libscap/scap_open.h
@@ -28,6 +28,7 @@ limitations under the License.
 #include "scap_procs.h"
 #include "../../driver/ppm_events_public.h"
 #include "../../driver/ppm_tp.h"
+#include "falcosecurity/log.h"
 
 #ifdef __cplusplus
 extern "C"
@@ -96,7 +97,7 @@ extern "C"
 									 // values via scap_suppress_events_comm().
 		interesting_ppm_sc_set ppm_sc_of_interest; ///< syscalls of interest.
 		interesting_tp_set tp_of_interest; ///< tp of interest. If left empty, no tracepoints will be attached
-		void(*debug_log_fn)(const char* msg); //< Function which SCAP may use to log a debug message
+		falcosecurity_log_fn log_fn; //< Function which SCAP may use to log messages
 		uint64_t proc_scan_timeout_ms; //< Timeout in msec, after which so-far-successful scan of /proc should be cut short with success return
 		uint64_t proc_scan_log_interval_ms; //< Interval for logging progress messages from /proc scan
 		void* engine_params;			   ///< engine-specific params.
