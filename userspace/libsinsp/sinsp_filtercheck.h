7356819d2444714637c1689d666d1cce6b00c900
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2024-08-02 11:12:12 +0200

    Expose extract_single in filterchecks (#80)

--- userspace/libsinsp/sinsp_filtercheck.h
+++ userspace/libsinsp/sinsp_filtercheck.h
@@ -174,7 +174,8 @@ public:
 	                       ppm_print_format print_format,
 	                       uint32_t len);
 
-	virtual uint8_t* extract(sinsp_evt*, OUT uint32_t* len, bool sanitize_strings = true);
+	// \param len [out] length in bytes for the returned value
+	virtual uint8_t* extract_single(sinsp_evt*, uint32_t* len, bool sanitize_strings = true);
 
 protected:
 	virtual bool compare_nocache(sinsp_evt*);
@@ -200,8 +201,6 @@ protected:
 	virtual bool extract_nocache(sinsp_evt* evt,
 	                             std::vector<extract_value_t>& values,
 	                             bool sanitize_strings = true);
-	// \param len [out] length in bytes for the returned value
-	virtual uint8_t* extract_single(sinsp_evt*, uint32_t* len, bool sanitize_strings = true);
 
 	bool compare_rhs(cmpop op, ppm_param_type type, const void* operand1, uint32_t op1_len = 0);
 	bool compare_rhs(cmpop op, ppm_param_type type, std::vector<extract_value_t>& values);
